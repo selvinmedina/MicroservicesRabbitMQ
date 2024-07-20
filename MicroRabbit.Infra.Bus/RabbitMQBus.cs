c3237864b7dbe3544c53b33bde2a636a7f1d2c80
Selvin Medina <selvinmedina0016@gmail.com>
2024-07-19 20:46:09 -0600

    Event Bus infrastructure

---
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace MicroRabbit.Infra.Bus;
+
+public sealed class RabbitMQBus : IEventBus
+{
+    private readonly IMediator _mediator;
+    private readonly Dictionary<string, List<Type>> _handlers;
+    private readonly List<Type> _eventTypes;
+
+    public RabbitMQBus(IMediator mediator)
+    {
+        _mediator = mediator;
+        _handlers = new Dictionary<string, List<Type>>();
+        _eventTypes = new List<Type>();
+    }
+
+    public void Publish<T>(T @event) where T : Event
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "selvin",
+            Password = "admin",
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        var eventName = @event.GetType().Name;
+
+        channel.QueueDeclare(eventName, false, false, false, null);
+
+        var message = JsonConvert.SerializeObject(@event);
+
+        var body = Encoding.UTF8.GetBytes(message);
+        
+        channel.BasicPublish("", eventName, null, body);
+    }
+
+    public Task SendCommand<T>(T command) where T : Command
+    {
+        return _mediator.Send(command);
+    }
+
+    public void Subscribe<T, TH>()
+        where T : Event
+        where TH : IEventHandler<T>
+    {
+        throw new NotImplementedException();
+    }
+}
